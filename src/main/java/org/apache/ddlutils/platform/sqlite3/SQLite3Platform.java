af3c0baad4c898b690c43612ef860cfa53a4cece
tohosaku <ny@cosmichorror.org>
2013-04-07 01:31:21 +0900

    add support for sqlite3
    some code copied from
    http://sourceforge.net/p/symmetricds/code/7054/tree/tags/symmetric-build-2.5.13/symmetric-ddl/src/

---
+++ src/main/java/org/apache/ddlutils/platform/sqlite3/SQLite3Platform.java
@@ -0,0 +1,104 @@
+package org.apache.ddlutils.platform.sqlite3;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Types;
+import java.util.Iterator;
+import java.util.Map;
+
+import org.apache.commons.beanutils.DynaBean;
+import org.apache.ddlutils.DatabaseOperationException;
+import org.apache.ddlutils.PlatformInfo;
+import org.apache.ddlutils.alteration.AddColumnChange;
+import org.apache.ddlutils.alteration.ModelComparator;
+import org.apache.ddlutils.alteration.RemoveColumnChange;
+import org.apache.ddlutils.alteration.TableChange;
+import org.apache.ddlutils.alteration.TableDefinitionChangesPredicate;
+import org.apache.ddlutils.dynabean.SqlDynaProperty;
+import org.apache.ddlutils.model.Column;
+import org.apache.ddlutils.model.Database;
+import org.apache.ddlutils.model.Table;
+import org.apache.ddlutils.platform.CreationParameters;
+import org.apache.ddlutils.platform.DefaultTableDefinitionChangesPredicate;
+import org.apache.ddlutils.platform.PlatformImplBase;
+
+/**
+ * The platform implementation for SQLite3.
+ * 
+ * @version $Revision: $
+ */
+public class SQLite3Platform extends PlatformImplBase
+{
+    /** Database name of this platform. */
+    public static final String DATABASENAME      = "SQLite3";
+    /** The standard SQLite jdbc driver. */
+    public static final String JDBC_DRIVER       = "org.sqlite.JDBC";
+    /** The subprotocol used by the standard SQLite driver. */
+    public static final String JDBC_SUBPROTOCOL  = "sqlite";
+
+    /**
+     * Creates a new platform instance.
+     */
+    public SQLite3Platform()
+    {
+        PlatformInfo info = getPlatformInfo();
+
+        info.setIdentityOverrideAllowed(false);
+        info.setSystemForeignKeyIndicesAlwaysNonUnique(true);
+        info.setNullAsDefaultValueRequired(false);
+        info.addNativeTypeMapping(Types.ARRAY, "BINARY", Types.BINARY);
+        info.addNativeTypeMapping(Types.DISTINCT, "BINARY", Types.BINARY);
+        info.addNativeTypeMapping(Types.NULL, "BINARY", Types.BINARY);
+        info.addNativeTypeMapping(Types.REF, "BINARY", Types.BINARY);
+        info.addNativeTypeMapping(Types.STRUCT, "BINARY", Types.BINARY);
+        info.addNativeTypeMapping(Types.DATALINK, "BINARY", Types.BINARY);
+
+        info.addNativeTypeMapping(Types.BIT, "BOOLEAN", Types.BIT);
+        info.addNativeTypeMapping(Types.TINYINT, "SMALLINT", Types.TINYINT);
+        info.addNativeTypeMapping(Types.SMALLINT, "SMALLINT", Types.SMALLINT);
+        info.addNativeTypeMapping(Types.BINARY, "BINARY", Types.BINARY);
+        info.addNativeTypeMapping(Types.BLOB, "BLOB", Types.BLOB);
+        info.addNativeTypeMapping(Types.CLOB, "CLOB", Types.CLOB);
+        info.addNativeTypeMapping(Types.FLOAT, "DOUBLE", Types.DOUBLE);
+        info.addNativeTypeMapping(Types.JAVA_OBJECT, "OTHER");
+
+        info.setDefaultSize(Types.CHAR, Integer.MAX_VALUE);
+        info.setDefaultSize(Types.VARCHAR, Integer.MAX_VALUE);
+        info.setDefaultSize(Types.BINARY, Integer.MAX_VALUE);
+        info.setDefaultSize(Types.VARBINARY, Integer.MAX_VALUE);
+
+        setSqlBuilder(new SQLite3Builder(this));
+        setModelReader(new SQLite3ModelReader(this));
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public String getName()
+    {
+        return DATABASENAME;
+    }
+}
